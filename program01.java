7bfd2b5b4ec82de4d15d9285c1f73ac143f06474
Maruthi P <maruthip266@gmail.com>
2025-09-11 01:33:46 +0530

    Add files via upload
    Find the range and size of a datatypes program

---
+++ program01.java
@@ -0,0 +1,46 @@
+package September;
+
+import java.util.Scanner;
+
+public class program01 {
+//Find the range and size of a datatypes
+	public static void main(String[] args) {
+		System.out.println("Byte Minimum range : "+Byte.MIN_VALUE);
+		System.out.println("Byte Maximum range : "+Byte.MAX_VALUE);
+		System.out.println("Byte size is : "+Byte.BYTES);
+		System.out.println("Byte bytes : "+Byte.SIZE);
+		System.out.println("------------------------------------------------------");
+		System.out.println("Short Minimum range : "+Short.MIN_VALUE);
+		System.out.println("Short Maximum range : "+Short.MAX_VALUE);
+		System.out.println("Short size is : "+Short.BYTES);
+		System.out.println("Short bytes : "+Short.SIZE);
+		System.out.println("------------------------------------------------------");
+		System.out.println("Integer bytes : "+Integer.BYTES);
+		System.out.println("Integer Minimum range : "+Integer.MIN_VALUE);
+		System.out.println("Integer Maximum range : "+Integer.MAX_VALUE);
+		System.out.println("Integer size is : "+Integer.SIZE);
+		System.out.println("------------------------------------------------------");
+		System.out.println("Long Minimum range : "+Long.MIN_VALUE);
+		System.out.println("Long Maximum range : "+Long.MAX_VALUE);
+		System.out.println("Long size is : "+Long.BYTES);
+		System.out.println("Long bytes : "+Long.SIZE);
+		System.out.println("------------------------------------------------------");
+		System.out.println("Float Minimum range : "+Float.MIN_VALUE);
+		System.out.println("Float Maximum range : "+Float.MAX_VALUE);
+		System.out.println("Float Bytes : "+Float.BYTES);
+		System.out.println("Float Size is : "+Float.SIZE);
+		System.out.println("------------------------------------------------------");
+		System.out.println("Double Minimum range : "+Double.MIN_VALUE);
+		System.out.println("Double Maximum range : "+Double.MAX_VALUE);
+		System.out.println("Double Bytes : "+Double.BYTES);
+		System.out.println("Double Size is : "+Double.SIZE);
+		System.out.println("------------------------------------------------------");
+		System.out.println("Char Minimum range : "+Character.MIN_VALUE);
+		System.out.println("Char Maximum range : "+Character.MAX_VALUE);
+		System.out.println("Char Bytes : "+Character.BYTES);
+		System.out.println("Char Size is : "+Character.SIZE);
+		System.out.println("-----------------------End-----------------------------");
+	
+	}
+
+}
