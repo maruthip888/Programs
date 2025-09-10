b8052366e9564c761e061aa58f0992b92d733bcd
Maruthi P <maruthip266@gmail.com>
2025-09-11 01:36:14 +0530

    Add files via upload
    Explicit conversion of data types

---
+++ program03.java
@@ -0,0 +1,16 @@
+package September;
+
+public class program03 {
+
+	public static void main(String[] args) {
+		int number1=20;
+		int number2=10;
+		int reminder=number1%number2;
+		System.out.println(number1/number2);
+		//Explicit converstion
+		float c=(float)number1/number2;
+		System.out.println(c);
+		System.out.println(reminder);
+	}
+
+}
