b6471122f051c31757c46c1ca6dabbf423c44f42
Maruthi P <maruthip266@gmail.com>
2025-09-13 21:21:33 +0530

    Add files via upload
    programs on constructor

---
+++ program08.java
@@ -0,0 +1,17 @@
+package September;
+
+public class program08 {
+	public static void display() {
+		System.out.println("Hello bro");
+	}
+	public static void write() {
+		System.out.println("my name is maruthi");
+	}
+	public static void main(String[] args) {
+	display();
+	write();
+	
+
+	}
+
+}
