e964b425f1f527dae5573466ef73410f33baf979
Maruthi P <maruthip266@gmail.com>
2025-09-14 21:28:58 +0530

    Add files via upload
    Largest number of the there values

---
+++ program15.java
@@ -0,0 +1,20 @@
+package September;
+
+import java.util.Scanner;
+
+public class program15 {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter first number: ");
+	        int a = sc.nextInt();
+	        System.out.print("Enter second number: ");
+	        int b = sc.nextInt();
+	        System.out.print("Enter third number: ");
+	        int c = sc.nextInt();
+
+	        int largest = (a>b) ? (a>c?a:c) : (b>c?b:c);
+	        System.out.println("Largest number is " + largest);
+	    }
+	
+
+}
